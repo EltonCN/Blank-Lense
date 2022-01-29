beacb9efc46c2d46656914a85dfbe014783d8894
EltonCN <43186596+EltonCN@users.noreply.github.com>
2022-01-29 17:57:10 -0300

    Teste de input com o novo sistema

--- ggj2022/Assets/Scripts/Player3DMoviment.cs
+++ ggj2022/Assets/Scripts/Player3DMoviment.cs
@@ -6,7 +6,11 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody))]
 public class Player3DMoviment : MonoBehaviour
 {
+    [SerializeField] float speed = 1f;
     Rigidbody rb;
+
+    bool moving = false;
+    Vector2 direction;
 
     // Start is called before the first frame update
     void Start()
@@ -14,21 +18,30 @@ public class Player3DMoviment : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if(moving)
+        {
+            Vector3 playerInput3D = new Vector3(direction.x, 0.0f, direction.y);
+            Vector3 step = speed * playerInput3D * Time.deltaTime;
+            Vector3 finalPosition = step + this.transform.position;
+            rb.MovePosition(finalPosition);
+        }
+        
     }
 
     public void Move(InputAction.CallbackContext context)
     {
-        Vector2 playerInput = context.ReadValue<Vector2>();
+        if(context.started || context.performed)
+        {
+            moving = true;
+            direction = context.ReadValue<Vector2>();
+        }
+        else
+        {
+            moving = false;
+        }
 
-        Vector3 playerInput3D = new Vector3(playerInput.x, 0.0f, playerInput.y);
-
-        Vector3 step = 50f * playerInput3D * Time.deltaTime;
-
-        Vector3 finalPosition = step + this.transform.position;
-
-        rb.MovePosition(finalPosition);
+        
     }
 }
